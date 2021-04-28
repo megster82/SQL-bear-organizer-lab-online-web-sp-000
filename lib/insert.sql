6164cb62225b45693ddc46ec76ca72290809e433
meaghan jennings <meaghanljennings@gmail.com>
2021-04-28 18:34:38 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, color) VALUES ('Mr. Chocolate', 4, 'brown');
+INSERT INTO bears (name, age, color) VALUES ('Rowdy', 2, 'black');
+INSERT INTO bears (name, age, color) VALUES ('Tabitha', 1, 'brown');
+INSERT INTO bears (name, age, color) VALUES ('Sergeant Brown', 4, 'black');
+INSERT INTO bears (name, age, color) VALUES ('Melissa', 3, 'brown');
+INSERT INTO bears (name, age, color) VALUES ('Grinch', 8, 'brown');
+INSERT INTO bears (name, age, color) VALUES ('Wendy', 7, 'black');
+INSERT INTO bears (name, age, color) VALUES (NULL, 6, 'black');
